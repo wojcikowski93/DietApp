be4dabc03b097cd8e7af3d273a86a4a03c4096ac
Kamil Wojcikowski <wojcikowski93@gmail.com>
2019-01-23 19:53:41 +0100

    Main Activity

---
+++ app/src/main/java/com/wojcikowski/kamil/dietapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.wojcikowski.kamil.dietapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBt, logInBt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerBt = findViewById(R.id.signIn);
+        logInBt = findViewById(R.id.logIn);
+
+        registerBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        logInBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: startActivity(new Intent(MainActivity.this, LogInActivity.class));
+            }
+        });
+    }
+}
