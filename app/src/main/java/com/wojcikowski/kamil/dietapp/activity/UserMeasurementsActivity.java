9e8be471e71a5b539e2014ccdb6bb497a79b10cc
wojcikowski93 <wojcikowski93@gmail.com>
2019-02-24 19:57:26 +0100

    Measurements activity

---
+++ app/src/main/java/com/wojcikowski/kamil/dietapp/activity/UserMeasurementsActivity.java
@@ -0,0 +1,15 @@
+package com.wojcikowski.kamil.dietapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wojcikowski.kamil.dietapp.R;
+
+public class UserMeasurementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_measurements);
+    }
+}
